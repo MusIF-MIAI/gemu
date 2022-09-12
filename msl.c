f4b108e59d99b79d7b110eadbccbf26fba6145e0
Antonio Willy Malara <antonio.malara@gmail.com>
2022-09-12 17:53:54 +0200

    Comment out an improper use of the comma operator

--- msl.c
+++ msl.c
@@ -8,5 +8,8 @@ static struct msl_cell MSL[MAX_CLOCK][MAX_STATUS];
 
 struct msl_cell *msl_get_cell(enum clock clock, uint8_t status)
 {
-    return MSL[clock, status];
+    //return MSL[clock, status];
+
+    MSL[0][0] = MSL[0][0];
+    return 0;
 }
