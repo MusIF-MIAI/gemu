9da5dd628b7889236f14731e16ab519d476430d7
Antonio Willy Malara <antonio.malara@gmail.com>
2022-09-12 17:55:42 +0200

    Run an entire cycle with the actual MSL

--- ge.c
+++ ge.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "ge.h"
 #include "msl.h"
-
+#include "msl-timings.h"
 
 static int ge_halted(struct ge *ge)
 {
@@ -44,6 +44,57 @@ void ge_start(struct ge * ge)
     ge->rSO = 0x80;
 
     ge->halted = 0;
+}
+
+static void ge_print_well_known_states(uint8_t state) {
+    switch (state) {
+        case 0x00: printf("\nState 00 - Display sequence\n\n"); break;
+        case 0x08: printf("\nState 08 - Forcing sequence\n\n"); break;
+        case 0x64:
+        case 0x65: printf("\nState 64+65 - Beta Phase\n\n"); break;
+        case 0x80: printf("\nState 80 - Initialitiation\n\n"); break;
+        case 0xE2:
+        case 0xE3: printf("\nState E2+E3 - Alpha Phase\n\n"); break;
+        case 0xF0: printf("\nState F0 - Interruption\n\n"); break;
+    }
+}
+
+static int ge_run_state(struct ge *ge)
+{
+    struct msl_timing_state *state;
+    struct msl_timing_chart *chart;
+
+    int old_SO = ge->rSO;
+
+    ge_print_well_known_states(ge->rSO);
+    printf("Running state %02X\n", ge->rSO);
+
+    state = &msl_timings[ge->rSO];
+
+    if (!state->count || !state->chart) {
+        printf("no timing charts found for state %02X\n", ge->rSO);
+        return 1;
+    }
+
+    for(ge->current_clock = TO00; ge->current_clock < MAX_CLOCK; ge->current_clock++) {
+        for (int i = 0; i < state->count; i++) {
+            chart = &state->chart[i];
+
+            if (chart->clock == ge->current_clock) {
+                if (chart->condition && !chart->condition(ge))
+                    continue;
+
+                chart->command(ge);
+            }
+        }
+    }
+
+    if (ge->rSO == old_SO) {
+        printf("State register SO did not change during an entire cycle, stopping emulation\n");
+        return 1;
+    }
+
+    return 0;
 }
 
 int ge_run_cycle(struct ge *ge)
