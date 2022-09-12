77e9da6d5d6bf6c145d30e6993102135dc25ea73
Antonio Willy Malara <antonio.malara@gmail.com>
2022-09-12 17:54:57 +0200

    More fleshed out implementation of the MSL

---
+++ msl-commands.h
@@ -0,0 +1,49 @@
+#ifndef MSL_COMMANDS_H
+#define MSL_COMMANDS_H
+
+#include "ge.h"
+
+void CI01(struct ge*);
+void CI02(struct ge*);
+void CI05(struct ge*);
+void CI06(struct ge*);
+void CI08(struct ge*);
+void CI12(struct ge*);
+void CI19(struct ge*);
+void CI32(struct ge*);
+void CI38(struct ge*);
+void CI39(struct ge*);
+void CI60(struct ge*);
+void CI62(struct ge*);
+void CI65(struct ge*);
+void CI67(struct ge*);
+void CI76(struct ge*);
+void CI80(struct ge*);
+void CI81(struct ge*);
+void CI82(struct ge*);
+void CI83(struct ge*);
+void CI89(struct ge*);
+
+void CO00(struct ge*);
+void CO02(struct ge*);
+void CO10(struct ge*);
+void CO12(struct ge*);
+void CO30(struct ge*);
+void CO41(struct ge*);
+void CO96(struct ge*);
+void CO97(struct ge*);
+
+void CU00(struct ge*);
+void CU01(struct ge*);
+void CU02(struct ge*);
+void CU03(struct ge*);
+void CU04(struct ge*);
+void CU05(struct ge*);
+void CU06(struct ge*);
+void CU10(struct ge*);
+void CU11(struct ge*);
+void CU17(struct ge*);
+
+void NONE(struct ge*);
+
+#endif /* MSL_COMMANDS_H */
