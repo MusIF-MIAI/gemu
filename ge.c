c234241f6943acdf8fbee45a904e4f09e8e3e7ae
Antonio Willy Malara <antonio.malara@gmail.com>
2023-02-26 09:25:55 +0100

    log: fix log of ni in reg dump

--- ge.c
+++ ge.c
@@ -108,7 +108,7 @@ void ge_print_registers_nonverbose(struct ge *ge)
     if (ge_log_enabled(LOG_REGS_V)) return;
     ge_log(LOG_REGS,
            "SO: %02x SA: %02x PO: %04x RO: %04x BO: %04x FO: %04x -  "
-           "V1: %04x  V2: %04x  V3: %04x  V4: %04x - "
+           "V1: %04x  V2: %04x V3: %04x  V4: %04x - "
            "L1: %04x  L2: %04x L3 : %04x\n",
            ge->rSO, ge->rSA, ge->rPO, ge->rRO, ge->rBO, ge->rFO,
            ge->rV1, ge->rV2, ge->rV3, ge->rV4,
@@ -121,11 +121,11 @@ void ge_print_registers_verbose(struct ge *ge)
            "%s:  "
            "SO: %02x SA: %02x PO: %04x RO: %04x BO: %04x FO: %04x  -  "
            "NO: %02x NI: %02x  -  "
-           "V1: %04x  V2: %04x  V3: %04x  V4: %04x - "
+           "V1: %04x  V2: %04x V3: %04x  V4: %04x - "
            "L1: %04x  L2: %04x L3 : %04x\n",
            ge_clock_name(ge->current_clock),
            ge->rSO, ge->rSA, ge->rPO, ge->rRO, ge->rBO, ge->rFO,
-           NO_knot(ge), ge->kNI,
+           NO_knot(ge), NI_knot(ge),
            ge->rV1, ge->rV2, ge->rV3, ge->rV4,
            ge->rL1, ge->rL2, ge->rL3);
 }
