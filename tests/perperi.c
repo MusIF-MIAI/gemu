a1c6d2e45b15ef09e85acfdd0da5744ecd9c2590
Antonio Willy Malara <antonio.malara@gmail.com>
2023-07-17 00:40:58 +0200

    tests: add more asserts to per-peri tests

--- tests/perperi.c
+++ tests/perperi.c
@@ -2,6 +2,7 @@
 
 #include "utest.h"
 #include "../ge.h"
+#include "../bit.h"
 
 #define ASSERT_CYCLE(x, name) \
     do {                      \
@@ -12,19 +13,35 @@
 
 UTEST(peri, per_peri) {
     /* first instruction of software/loader.txt */
-    uint8_t up_name = 0x80; // connector 2 - 
+
+    uint8_t up_name = 0x80; /* connector 2 */
     uint8_t addr_hi = 0x00;
     uint8_t addr_lo = 0x10;
-    
+    uint8_t z  = 0x00;
+    uint8_t x  = 0xbb; /* 0x40; */
+    uint8_t l1 = 0xcc; /* 0x00; */
+    uint8_t l2 = 0xdd; /* 0x4f; */
+    uint8_t i1 = 0xee; /* 0x00; */
+    uint8_t i2 = 0xff; /* 0x28; */
+
     uint16_t addr  = (addr_hi << 8) | addr_lo;
-    uint8_t  mem[] = { PER_OPCODE, up_name, addr_hi, addr_lo };
-    
+
     struct ge g;
     struct ge_console c;
     
     ge_init(&g);
-    ge_load_program(&g, mem, sizeof(mem));
-    
+
+    g.mem[0x00] = PER_OPCODE;
+    g.mem[0x01] = up_name;
+    g.mem[0x02] = addr_hi;
+    g.mem[0x03] = addr_lo;
+    g.mem[0x10] = z;
+    g.mem[0x11] = x;
+    g.mem[0x12] = l1;
+    g.mem[0x13] = l2;
+    g.mem[0x14] = i1;
+    g.mem[0x15] = i2;
+
     ge_clear(&g);
     ge_start(&g);
     
@@ -43,13 +60,21 @@ UTEST(peri, per_peri) {
     
     ASSERT_CYCLE(0xc8, "PER-PERI 1");
     ASSERT_EQ(g.rV2, addr + 1);
-    
+    ASSERT_EQ(g.rL2, z);
+    ASSERT_FALSE(BIT(g.ffFI, 4));
+
     ASSERT_CYCLE(0xd8, "PER-PERI 2");
+    ASSERT_EQ(g.rRE, up_name);
+
     ASSERT_CYCLE(0xd9, "PER-PERI 3");
     ASSERT_CYCLE(0xda, "PER-PERI 4");
     ASSERT_CYCLE(0xdb, "PER-PERI 5");
     ASSERT_CYCLE(0xdc, "PER-PERI 6");
+    ASSERT_EQ(g.rV1, g.rV3);
+
     ASSERT_CYCLE(0xcc, "PER-PERI 7");
+    ASSERT_EQ(g.rRE, x);
+
     ASSERT_CYCLE(0xca, "TPER-CPER 1");
     ASSERT_CYCLE(0xa8, "TPER-CPER 2");
     ASSERT_CYCLE(0xa9, "TPER-CPER 3");
