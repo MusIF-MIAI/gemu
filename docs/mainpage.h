f285bad22c074166fd3e27a170a7a46bd08375c0
Antonio Willy Malara <antonio.malara@gmail.com>
2023-12-07 20:19:14 +0100

    tooling: build console for github pages

---
+++ docs/mainpage.h
@@ -0,0 +1,12 @@
+/*!
+ * \mainpage GE-115 Emulator Documentation
+ *
+ * This is an early stage work in progress emulator for the GE-115 family
+ * of General Electric mainframe computers. The main scope of the  project
+ * is exploring the architecture of the computer in order to aid the
+ * restoration of an actual computer,
+ *
+ * A totally for fun implementation of the maintenance console can be found
+ * <a href="emu">here</a>.
+ */
+
