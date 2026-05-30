6c64c6297ec3ea3c6d5ecb0554b482e311958a96
danielinux <danielinux@users.noreply.github.com>
2026-05-30 22:07:32 +0000

    deploy: 32d89582b2f74bb1e69e39e96ebad463f5a3ead7

--- menudata.js
+++ menudata.js
@@ -119,6 +119,7 @@ var menudata={children:[
 {text:"a",url:"globals_vars.html#index_a"},
 {text:"c",url:"globals_vars.html#index_c"},
 {text:"d",url:"globals_vars.html#index_d"},
+{text:"g",url:"globals_vars.html#index_g"},
 {text:"h",url:"globals_vars.html#index_h"},
 {text:"m",url:"globals_vars.html#index_m"},
 {text:"p",url:"globals_vars.html#index_p"},
